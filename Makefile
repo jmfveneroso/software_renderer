a6b073266f8fe1d89fac148e6876219936a1aac0
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2017-08-21 23:43:49 -0300

    Hardware acceleration.

--- Makefile
+++ Makefile
@@ -6,12 +6,10 @@ ifeq ($(OS), Linux)
   INC=-I/usr/local/include
   LIBS=-L/usr/X11/lib -L/usr/local/lib -lglfw3 -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -lGLEW -lpng
 else
-  LIBS=-L/usr/X11/lib -L/usr/local/lib -lglfw -lglew -lpng -framework OpenGL -framework GLUT
+  LIBS=-L/usr/local/lib -lglfw -lglew -framework OpenGL -framework GLUT
 endif
 
-
-
-_DEPS = src/alternative_render.h src/visual_engine.h src/vector.h src/vertex.h src/bitmap.h src/objloader.h
+_DEPS = src/shaders.h src/bitmap.h src/objloader.h
 
 build/main: src/main.cpp $(_DEPS)
 	$(CC) -o $@ $< $(LIBS) $(C_FLAGS) $(INC) -g -O3
--- src/main.cpp
+++ src/main.cpp
@@ -17,131 +17,30 @@ using namespace std;
 #include <glm/gtx/transform.hpp>
 using namespace glm;
 
-#include "visual_engine.h"
-#include "alternative_render.h"
+#include "shaders.h"
+#include "bitmap.h"
+#include "controls.h"
+#include "objloader.h"
+#include "vbo_indexer.h"
 
-GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path){
-  // Create the shaders
-  GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-  GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+GLuint CreateMvpMatrix(GLuint programID, glm::mat4* MVP) {
+  GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-  // Read the Vertex Shader code from the file
-  std::string VertexShaderCode;
-  std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-  if(VertexShaderStream.is_open()){
-    std::string Line = "";
-    while(getline(VertexShaderStream, Line))
-      VertexShaderCode += "\n" + Line;
-    VertexShaderStream.close();
-  }else{
-    printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
-    getchar();
-    return 0;
-  }
+  // Projection matrix.
+  glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
 
-  // Read the Fragment Shader code from the file
-  std::string FragmentShaderCode;
-  std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-  if(FragmentShaderStream.is_open()){
-    std::string Line = "";
-    while(getline(FragmentShaderStream, Line))
-      FragmentShaderCode += "\n" + Line;
-    FragmentShaderStream.close();
-  }
+  glm::mat4 View = glm::lookAt(
+    glm::vec3(4, 3, 3), // Camera position in World Space.
+    glm::vec3(0, 0, 0), // Looking to location.
+    glm::vec3(0, 1, 0)  // Head is up (set to 0, -1, 0 to look upside-down)
+  );
 
-  GLint Result = GL_FALSE;
-  int InfoLogLength;
+  // Model matrix : an identity matrix (model will be at the origin)
+  glm::mat4 Model = glm::mat4(1.0f);
 
-  // Compile Vertex Shader
-  printf("Compiling shader : %s\n", vertex_file_path);
-  char const * VertexSourcePointer = VertexShaderCode.c_str();
-  glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
-  glCompileShader(VertexShaderID);
-
-  // Check Vertex Shader
-  glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-  glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-  if ( InfoLogLength > 0 ){
-    std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
-    glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-    printf("%s\n", &VertexShaderErrorMessage[0]);
-  }
-
-  // Compile Fragment Shader
-  printf("Compiling shader : %s\n", fragment_file_path);
-  char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-  glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
-  glCompileShader(FragmentShaderID);
-
-  // Check Fragment Shader
-  glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-  glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-  if ( InfoLogLength > 0 ){
-    std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
-    glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-    printf("%s\n", &FragmentShaderErrorMessage[0]);
-  }
-
-  // Link the program
-  printf("Linking program\n");
-  GLuint ProgramID = glCreateProgram();
-  glAttachShader(ProgramID, VertexShaderID);
-  glAttachShader(ProgramID, FragmentShaderID);
-  glLinkProgram(ProgramID);
-
-  // Check the program
-  glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-  glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-  if ( InfoLogLength > 0 ){
-    std::vector<char> ProgramErrorMessage(InfoLogLength+1);
-    glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-    printf("%s\n", &ProgramErrorMessage[0]);
-  }
-
-  
-  glDetachShader(ProgramID, VertexShaderID);
-  glDetachShader(ProgramID, FragmentShaderID);
-  
-  glDeleteShader(VertexShaderID);
-  glDeleteShader(FragmentShaderID);
-
-  return ProgramID;
-}
-
-GLuint CreateTexture(int x) {
-  GLuint texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-
-  // bool even = true;
-  // int counter = 0;
-  // for (int i = 0; i < windowHeight; ++i) {
-  //   for (int j = 0; j < windowWidth; ++j) {
-  //     if (i % x < x/2) {
-  //       data[i * windowWidth * 3 + j * 3]     = 0;
-  //       data[i * windowWidth * 3 + j * 3 + 1] = 0;
-  //       data[i * windowWidth * 3 + j * 3 + 2] = 255;
-  //     } else {
-  //       data[i * windowWidth * 3 + j * 3]     = 255;
-  //       data[i * windowWidth * 3 + j * 3 + 1] = 255;
-  //       data[i * windowWidth * 3 + j * 3 + 2] = 255;
-  //     }
-  //     even = !even;
-  //   }
-  // }
-
-  // for (int j = 0; j < windowWidth; ++j) {
-  //   DrawPixel(j, 100, 0, 0, 255);
-  // }
-
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, windowWidth, windowHeight, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-  
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-  return texture;
+  // Our ModelViewProjection : multiplication of our 3 matrices
+  *MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+  return MatrixID;
 }
 
 GLFWwindow* window;
@@ -158,13 +57,11 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  int windowWidth = 1000;
-  int windowHeight = 600;
-  data = new unsigned char[windowWidth * windowHeight * 3];
+  int windowWidth = 600;
+  int windowHeight = 400;
   window = glfwCreateWindow(windowWidth, windowHeight, "Test", NULL, NULL);
   if( window == NULL ){
-    fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-    getchar();
+    fprintf( stderr, "Failed to open GLFW window.\n" );
     glfwTerminate();
     return -1;
   }
@@ -174,7 +71,7 @@ int main() {
   glfwGetFramebufferSize(window, &windowWidth, &windowHeight);
   glfwMakeContextCurrent(window);
 
-  glewExperimental = GL_TRUE; 
+  glewExperimental = true; // Needed for core profile
   if (glewInit() != GLEW_OK) {
     fprintf(stderr, "Failed to initialize GLEW\n");
     getchar();
@@ -182,36 +79,72 @@ int main() {
     return -1;
   }
 
-  // Hide the mouse and enable unlimited mouvement
+  // Hide the mouse and enable unlimited movement
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwPollEvents();
+  glfwSetCursorPos(window, windowWidth/2, windowHeight/2);
+  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS); 
+  glEnable(GL_CULL_FACE);
   
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
   // Create and compile our GLSL program from the shaders
   GLuint programID = LoadShaders("shaders/vshade", "shaders/fshade");
+  
+  glm::mat4 MVP;
+  GLuint MatrixID = CreateMvpMatrix(programID, &MVP); 
+  GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
+  GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
+
+  GLuint Texture = loadBMP_custom("textures/dirt.bmp");
   GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
-  // The fullscreen quad's FBO
-  GLuint quad_VertexArrayID;
-  glGenVertexArrays(1, &quad_VertexArrayID);
-  glBindVertexArray(quad_VertexArrayID);
-  
-  static const GLfloat g_quad_vertex_buffer_data[] = {
-    -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f,  1.0f, 0.0f,
-    -1.0f,  1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
-  };
+  GLuint vertexbuffer, uvbuffer, normalbuffer, elementbuffer;
 
-  GLuint quad_vertexbuffer;
-  glGenBuffers(1, &quad_vertexbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
+  std::vector<glm::vec3> vertices;
+  std::vector<glm::vec2> uvs;
+  std::vector<glm::vec3> normals; // Won't be used at the moment.
+  bool res = loadOBJ("res/suzanne.obj", vertices, uvs, normals);
+
+  std::vector<unsigned short> indices;
+  std::vector<glm::vec3> indexed_vertices;
+  std::vector<glm::vec2> indexed_uvs;
+  std::vector<glm::vec3> indexed_normals;
+  indexVBO(vertices, uvs, normals, indices, indexed_vertices, indexed_uvs, indexed_normals);
+  
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
+  
+  glGenBuffers(1, &uvbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+  glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &indexed_uvs[0], GL_STATIC_DRAW);
+  
+  glGenBuffers(1, &normalbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+  glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &indexed_normals[0], GL_STATIC_DRAW);
+  
+  glGenBuffers(1, &elementbuffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0] , GL_STATIC_DRAW);
+
+
+
+
+
+
+
+
+
+  glUseProgram(programID);
+  GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
 
   double lastTime = glfwGetTime();
   int nbFrames = 0;
-
-  GLuint texture;
-  int counter = 2;
-
-  InitVisualEngine();
-  InitRender();
 
   do {
     // Measure speed
@@ -224,82 +157,155 @@ int main() {
       lastTime += 1.0;
     }
 
-    // Calculate motion.
-
-    // if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
-    //   player.pos.x += speed * cos(player.angle);
-    //   player.pos.y += speed * sin(player.angle);
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_DOWN ) == GLFW_PRESS){
-    //   player.pos.x -= speed * cos(player.angle);
-    //   player.pos.y -= speed * sin(player.angle);
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_RIGHT ) == GLFW_PRESS){
-    //   player.angle += 0.03;
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_LEFT ) == GLFW_PRESS){
-    //   player.angle -= 0.03;
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_Q ) == GLFW_PRESS){
-    //   player.height -= 20;
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
-    //   player.height += 20;
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_E ) == GLFW_PRESS){
-    //   player.v_angle -= 3;
-    //   // if (player.v_angle < -M_PI + 0.1) player.v_angle = -M_PI + 0.1;
-    // }
-
-    // if (glfwGetKey( window, GLFW_KEY_R ) == GLFW_PRESS){
-    //   player.v_angle += 3;
-    //   // if (player.v_angle > M_PI - 0.1) player.v_angle = M_PI - 0.1;
-    // }
-
     // Render to the screen
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glViewport(0, 0, windowWidth, windowHeight);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(programID);
+    computeMatricesFromInputs();
+    glm::mat4 ProjectionMatrix = getProjectionMatrix();
+    glm::mat4 ViewMatrix = getViewMatrix();
+    glm::mat4 ModelMatrix = glm::mat4(1.0);
+    glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
+    // Send our transformation to the currently bound shader, 
+    // in the "MVP" uniform
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+    glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+    glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
+    
+    glm::vec3 lightPos = glm::vec3(4,4,4);
+    glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
+
+    // Bind our texture in Texture Unit 0
     glActiveTexture(GL_TEXTURE0);
-
-    Render();
-    texture = CreateTexture(counter++); 
-
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindTexture(GL_TEXTURE_2D, Texture);
+    // Set our "myTextureSampler" sampler to use Texture Unit 0
     glUniform1i(TextureID, 0);
 
+    // 1rst attribute buffer : vertices
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(
-    	0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+    	0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
     	3,                  // size
     	GL_FLOAT,           // type
     	GL_FALSE,           // normalized?
     	0,                  // stride
     	(void*)0            // array buffer offset
     );
+    
+    // 2nd attribute buffer : uv
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glVertexAttribPointer(
+    	1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+    	2,                                // size
+    	GL_FLOAT,                         // type
+    	GL_FALSE,                         // normalized?
+    	0,                                // stride
+    	(void*)0                          // array buffer offset
+    );
 
-    glDrawArrays(GL_TRIANGLES, 0, 6); // 2*3 indices starting at 0 -> 2 triangles
+    // 3rd attribute buffer : normals
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+    glVertexAttribPointer(
+    	2,                                // attribute
+    	3,                                // size
+    	GL_FLOAT,                         // type
+    	GL_FALSE,                         // normalized?
+    	0,                                // stride
+    	(void*)0                          // array buffer offset
+    );
+    
+    // glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
+
+    // Index buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+    
+    // Draw the triangles !
+    glDrawElements(
+    	GL_TRIANGLES,      // mode
+    	indices.size(),    // count
+    	GL_UNSIGNED_SHORT,   // type
+    	(void*)0           // element array buffer offset
+    );
+    
+
+    ////// Start of the rendering of the second object //////
+    for (int i = 0; i < 100; i++) {
+      for (int j = 0; j < 10; j++) {
+        glm::mat4 ModelMatrix2 = glm::mat4(1.0);
+        ModelMatrix2 = glm::translate(ModelMatrix2, glm::vec3(2.0f + i * 2.0f, 0.0f, 2.0f + j * 2.0f));
+        glm::mat4 MVP2 = ProjectionMatrix * ViewMatrix * ModelMatrix2;
+        
+        // Send our transformation to the currently bound shader, 
+        // in the "MVP" uniform
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP2[0][0]);
+        glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix2[0][0]);
+        
+        
+        // The rest is exactly the same as the first object
+        
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        // 2nd attribute buffer : UVs
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        // 3rd attribute buffer : normals
+        glEnableVertexAttribArray(2);
+        glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        // Index buffer
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+        
+        // Draw the triangles !
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, (void*)0);
+      }
+    }
+    ////// End of rendering of the second object //////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(2);
 
     // Swap buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
-    glDeleteTextures(1, &texture);
   
   } // Check if the ESC key was pressed or the window was closed
   while (
     glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
     glfwWindowShouldClose(window) == 0 
   );
+
+  // Cleanup VBO and shader
+  glDeleteBuffers(1, &vertexbuffer);
+  glDeleteBuffers(1, &uvbuffer);
+  glDeleteBuffers(1, &normalbuffer);
+  glDeleteBuffers(1, &elementbuffer);
+  glDeleteProgram(programID);
+  glDeleteTextures(1, &Texture);
+  glDeleteVertexArrays(1, &VertexArrayID);
 
   // Close OpenGL window and terminate GLFW
   glfwTerminate();
